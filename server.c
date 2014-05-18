4115f947fd490b8d8b1ace6d093d9b142ee5afb2
woowong <noradvii@gmail.com>
2014-05-18 10:25:26 -0700

    server settings with multithread

--- server.c
+++ server.c
@@ -4,58 +4,82 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <pthread.h>
 
-void error_handling (char *msg);
+int connectSocket(int port);
+void* server_thread(void *args);
 
 int main (int argc, char *argv[])
 {
 	int serv_sock;
 	int clnt_sock;
 
-
 	struct sockaddr_in serv_addr;
 	struct sockaddr_in clnt_addr;
 	socklen_t clnt_addr_size;
+	pthread_t thread;
 
+	int tt=5;
 
-	char msg[] = "Hello World!";
-
-	if (argc!=2)
-	{
-		printf("Usage : %s <port>\n", argv[0]);
-		exit(1);
+	if (argc < 2) {
+		printf("Usage : %s <Port> \n", argv[0]);
+		return 0;
 	}
+	else {
+		
+		serv_sock = connectSocket(atoi(argv[1]));
 
-	serv_sock = socket(PF_INET, SOCK_STREAM, 0);
-	if (serv_sock == -1)
-		error_handling ("socket() error");
 
-	memset (&serv_addr, 0, sizeof(serv_addr));
+		for(;;)
+		{
+			clnt_addr_size = sizeof(clnt_addr);
+			printf("before accept\n");
+			clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+			printf("after accept\n");
+			if(clnt_sock==-1) {
+				printf("accept() error\n");
+				exit(-1);
+			}
+			
+			pthread_create(&thread, NULL, server_thread, (void*)&tt);
+			
+		}
+
+	}
+}
+
+void* server_thread (void *args)
+{
+	
+}
+
+// Socket Connection
+int connectSocket(int port)
+{
+	int sock;
+	struct sockaddr_in serv_addr;
+	struct sockaddr_in clnt_addr;
+	socklen_t clnt_addr_size;
+	
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock == -1) {
+		printf ("socket() error\n");
+		exit(-1);
+	}
+	
+	memset(&serv_addr, 0, sizeof(serv_addr));
 	serv_addr.sin_family=AF_INET;
 	serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);
-	serv_addr.sin_port=htons(atoi(argv[1]));
+	serv_addr.sin_port=htons(port);
 
-	if(bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1)
-		error_handling ("bind() error");
+	if(bind(sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1) {
+		printf ("bind() error\n"); 
+		exit(-1);
+	}
+	if (listen(sock, 5)== -1) {
+		printf("listen() error\n");
+		exit(-1);
+	}
 
-	if (listen(serv_sock, 5)== -1)
-		error_handling("listen() error");
-
-	clnt_addr_size = sizeof(clnt_addr);
-	clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
-	if(clnt_sock==-1)
-		error_handling("accept() error");
-
-	write(clnt_sock, msg, sizeof(msg));
-	close(clnt_sock);
-	close(serv_sock);
-	return 0;
+	return sock;
 }
-
-void error_handling (char *msg)
-{
-	fputs(msg, stderr);
-	fputc('\n', stderr);
-	exit(1);
-}
-
